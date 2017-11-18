458a4b80d017f42740d69d1ab4ab33130cc649fa
Shirong Zheng <shirongzheng1027@gmail.com>
2017-11-18 16:33:35 -0500

    fix errors

--- stockmarket/Call_Option.sql
+++ stockmarket/Call_Option.sql
@@ -1,6 +1,10 @@
+DELIMITER //
+
 CREATE PROCEDURE `Call_Option`(IN `Trader_ID` INT)
 BEGIN
+
     SELECT * FROM Call_Option WHERE `Trader_ID` = Trader_ID;
     SELECT * FROM option_transactions WHERE `seller_id` = Trader_ID;
-
-END
+   
+END //
+DELIMITER ;
