8b3d66ca87b59e5c376ddf387f810afd26d425e8
Shirong Zheng <shirongzheng1027@gmail.com>
2017-11-19 00:55:59 -0500

    show stock symbol

--- stockmarket/Call_Option.sql
+++ stockmarket/Call_Option.sql
@@ -2,9 +2,17 @@ DELIMITER //
 
 CREATE PROCEDURE `Call_Option`(IN `Trader_ID` INT)
 BEGIN
+BEGIN
 
-    SELECT * FROM Call_Option WHERE `Trader_ID` = Trader_ID;
+    DECLARE YourStock varchar(15);
+    
+    
+    SET YourStock=(SELECT `Stock` FROM Call_Option WHERE `Trader_ID` = Trader_ID);
     SELECT * FROM option_transactions WHERE `seller_id` = Trader_ID;
+    SELECT YourStock;
+    
+
+END
    
 END //
 DELIMITER ;
