c29aef49cb27d48fd51d0a052eea70b2318c466d
Shirong Zheng <shirongzheng1027@gmail.com>
2017-11-21 18:00:33 -0500

    Update Call_Option.sql

--- stockmarket/Call_Option.sql
+++ stockmarket/Call_Option.sql
@@ -2,17 +2,20 @@ DELIMITER //
 
 CREATE PROCEDURE `Call_Option`(IN `Trader_ID` INT)
 BEGIN
-BEGIN
-
+    DECLARE BuyerName varchar(15);
     DECLARE YourStock varchar(15);
+    DECLARE AmountOfStock INT(15);
+    DECLARE GainMoney INT(15);
     
-    
+    SET BuyerName=(SELECT `Name` FROM Call_Option WHERE `Trader_ID` = Trader_ID);
     SET YourStock=(SELECT `Stock` FROM Call_Option WHERE `Trader_ID` = Trader_ID);
-    SELECT * FROM option_transactions WHERE `seller_id` = Trader_ID;
-    SELECT YourStock;
+    SET AmountOfStock=(SELECT `trade_size` From option_transactions WHERE `buyer_id` = Trader_ID);
+    SET GainMoney =(SELECT `buyer_gain` FROM option_transactions WHERE `buyer_id` = Trader_ID);
     
-
-END
+    SELECT BuyerName;
+    SELECT YourStock;
+    SELECT AmountOfStock;
+    SELECT GainMoney;
    
 END //
 DELIMITER ;
