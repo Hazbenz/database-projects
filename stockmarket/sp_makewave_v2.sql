718dfbaa95166c8575cd4f8a4c6ec8004f003615
dxwc <dxwc@users.noreply.github.com>
2017-11-22 08:44:53 +0000

    update after stock table

--- stockmarket/sp_makewave_v2.sql
+++ stockmarket/sp_makewave_v2.sql
@@ -10,59 +10,85 @@ BEGIN
     DECLARE v_time_diff INT;
     DECLARE v_trade_price DOUBLE(18, 4);
 
-    SELECT INSTRUMENT_ID
-    INTO v_instrument_id
-    FROM stocks
-    ORDER BY RAND()
-    LIMIT 1;
+    DECLARE v_distinct_instrument_id BOOLEAN; -- non zero will be true
 
-    SELECT
-        TRADE_TIME,
-        TRADE_PRICE
-    INTO
-        v_trade_time,
-        v_trade_price
-    FROM
-        STOCK_TRADE
-    WHERE 
-        INSTRUMENT_ID = v_instrument_id
-    ORDER BY
-        RAND() LIMIT 1;
+    SELECT count(distinct INSTRUMENT_ID)
+    INTO v_distinct_instrument_id
+    FROM STOCK_TRADE;
 
-    SET v_time_diff = TIMESTAMPDIFF(
-                            MINUTE,
-                            v_trade_time,
-                            DATE_ADD(DATE(v_trade_time), INTERVAL 16 HOUR)
-                        );
+    IF v_distinct_instrument_id THEN
+        SELECT INSTRUMENT_ID
+        INTO v_instrument_id
+        FROM stocks
+        ORDER BY RAND()
+        LIMIT 1;
 
-    IF v_time_diff > 30 THEN
-        SET v_next_time = DATE_ADD(
-                            v_trade_time,
-                            INTERVAL (FLOOR(RAND() * v_time_diff) + 1) MINUTE
-                        );
+        SELECT
+            TRADE_TIME,
+            TRADE_PRICE
+        INTO
+            v_trade_time,
+            v_trade_price
+        FROM
+            STOCK_TRADE
+        WHERE 
+            INSTRUMENT_ID = v_instrument_id
+        ORDER BY
+            RAND() LIMIT 1;
+
+        SET v_time_diff = TIMESTAMPDIFF(
+                                MINUTE,
+                                v_trade_time,
+                                DATE_ADD(DATE(v_trade_time), INTERVAL 16 HOUR)
+                            );
+
+        IF v_time_diff > 30 THEN
+            SET v_next_time = DATE_ADD(
+                                v_trade_time,
+                                INTERVAL (FLOOR(RAND() * v_time_diff) + 1) MINUTE
+                            );
+        ELSE
+            SET v_next_time = DATE_ADD(
+                                DATE_ADD(v_trade_time, INTERVAL 1 DAY),
+                                INTERVAL (FLOOR(RAND() * 30) + 1) MINUTE
+                            );
+        END IF;
+
+        INSERT INTO STOCK_TRADE
+        (
+            INSTRUMENT_ID,
+            TRADE_DATE,
+            TRADE_TIME,
+            TRADE_PRICE,
+            TRADE_SIZE
+        )
+        VALUES
+        (
+            v_instrument_id,
+            DATE(v_next_time),
+            v_next_time,
+            ((v_trade_price * (FLOOR((RAND() * 7)-3))/100) + v_trade_price),
+            FLOOR((RAND() * 1000) + 1)
+        );
     ELSE
-        SET v_next_time = DATE_ADD(
-                            DATE_ADD(v_trade_time, INTERVAL 1 DAY),
-                            INTERVAL (FLOOR(RAND() * 30) + 1) MINUTE
-                        );
+        INSERT INTO STOCK_TRADE
+        (
+            INSTRUMENT_ID,
+            TRADE_DATE,
+            TRADE_TIME,
+            TRADE_PRICE,
+            TRADE_SIZE
+        )
+        SELECT
+            INSTRUMENT_ID,
+            DATE_SUB(DATE(NOW()), INTERVAL 7 YEAR),
+            DATE_ADD(DATE_SUB(DATE(NOW()), INTERVAL 7 YEAR), INTERVAL FLOOR((RAND() * 7)+9) HOUR),
+            FLOOR(RAND() * 100) + 1,
+            FLOOR(RAND() * 1000) + 1
+        FROM
+            stocks;
     END IF;
 
-    INSERT INTO STOCK_TRADE
-    (
-        INSTRUMENT_ID,
-        TRADE_DATE,
-        TRADE_TIME,
-        TRADE_PRICE,
-        TRADE_SIZE
-    )
-    VALUES
-    (
-        v_instrument_id,
-        DATE(v_next_time),
-        v_next_time,
-        ((v_trade_price * (FLOOR((RAND() * 7)-3))/100) + v_trade_price),
-        FLOOR((RAND() * 1000) + 1)
-    );
 
 END //
 DELIMITER ;
